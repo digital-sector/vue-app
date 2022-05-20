dc92ac234a0dc5ce859251f4ce202b5c7edbf22c
digital-sector <specificvector@protonmail.com>
2022-05-20 15:46:29 +1000

    General updates

--- vue/script.js
+++ vue/script.js
@@ -3,9 +3,25 @@ import { createApp } from 'vue'
 const app = createApp({
     data() {
       return {
+        cart: 0,
         product: 'Skins',
         image: "./imgs/steve.webp",
         inventory: 0,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+          { id: 2234, color: 'steve', image: './imgs/steve.webp' },
+          { id: 2235, color: 'alex', image: './imgs/alex.webp' }
+        
+        ],
+      }
+    },
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateImage(variantImage) {
+        this.image = variantImage
+
       }
     }
   }).mount('#app')
