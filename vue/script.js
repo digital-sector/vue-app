4eb2cfa3095719e14f0413c6ef401a4b4a5e1852
digital-sector <specificvector@protonmail.com>
2022-05-21 11:34:58 +1000

    added some computed properties.

--- vue/script.js
+++ vue/script.js
@@ -4,13 +4,14 @@ const app = createApp({
     data() {
       return {
         cart: 0,
+        brand: 'VueMastery',
         product: 'Skins',
-        image: "./imgs/steve.webp",
+        selectedVariant: 0,
         inventory: 10,
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
-          { id: 2234, color: 'green', image: './imgs/steve.webp' },
-          { id: 2235, color: 'blue', image: './imgs/alex.webp' }
+          { id: 2234, color: 'green', image: './imgs/steve.webp', quantity: 50 },
+          { id: 2235, color: 'blue', image: './imgs/alex.webp', quantity: 0 }
         
         ],
       }
@@ -19,9 +20,17 @@ const app = createApp({
       addToCart() {
         this.cart += 1
       },
-      updateImage(variantImage) {
-        this.image = variantImage
-
+      updateVariant(index) {
+        this.selectedVariant = index
+      }
+    },
+    computed: {
+      title() {
+        return this.brand + ' ' + this.product
+      },
+      image() {
+        return this.variants[this.selectedVariant]
       }
     }
+    
   }).mount('#app')
